7bdcde7b44664cd8b4991880170865303b417c27
Leonardo Braz <lhleonardo@hotmail.com>
2020-08-28 17:09:09 -0400

    Tabela genérica para qulalquer tipo de dado

--- web/src/components/Table/index.js
+++ web/src/components/Table/index.js
@@ -3,21 +3,36 @@ import React from "react";
 import { Container, ReportTable, Report } from "./styles";
 import PropTypes from "prop-types";
 
-function Table({ reports }) {
+/**
+ *
+ * @param {String[]} columns nome de cada coluna
+ * @param {String[]} rows mapeamento de cada linha da tabela
+ * @param {Array} data conteúdo da tabela
+ */
+function Table({ columns, rows, data, dangerValidate, warnValidate }) {
   return (
     <Container>
       <ReportTable>
         <thead>
           <tr>
-            <th>Mês/Ano</th>
-            <th>Esperado (CRESESB)</th>
-            <th>Produção</th>
-            <th>Percentual</th>
-            <th>Diferença</th>
+            {columns.map((column) => (
+              <th key={column}>{column}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {reports.map((report) => (
+          {data.map((item) => {
+            const cells = rows(item);
+
+            return (
+              <Report warn={warnValidate(item)} danger={dangerValidate(item)}>
+                {cells.map((cell) => (
+                  <td key={cell}>{cell}</td>
+                ))}
+              </Report>
+            );
+          })}
+          {/* {reports.map((report) => (
             <Report key={`${report.month}${report.year}`}
             warn={report.difference < (-15) && report.difference > (-25)}
             danger={report.difference < (-25)}>
@@ -27,13 +42,25 @@ function Table({ reports }) {
               <td>{report.percentual}%</td>
               <td>{report.difference}%</td>
             </Report>
-            ))}
+            ))} */}
         </tbody>
       </ReportTable>
     </Container>
-          )}
+  );
+}
+
+Table.defaultProps = {
+  dangerValidate: () => false,
+  warnValidate: () => false,
+};
+
 Table.propTypes = {
-  reports: PropTypes.array,
+  columns: PropTypes.arrayOf(PropTypes.string),
+  rows: PropTypes.func,
+  data: PropTypes.array,
+
+  dangerValidate: PropTypes.func,
+  warnValidate: PropTypes.func,
 };
 
 export default Table;

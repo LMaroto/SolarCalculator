ccff74b4a42870c6107872354f4243b312166c62
Leonardo Braz <lhleonardo@hotmail.com>
2020-10-05 21:10:26 -0400

    Correção de busca dos registros de leitura

--- backend/src/app/repositories/ReportRepository.js
+++ backend/src/app/repositories/ReportRepository.js
@@ -1,6 +1,21 @@
 import connection from '../../database';
 import dateOrdinate from '../utils/DateOrdinate';
 
+const months = {
+  jan: '01',
+  fev: '02',
+  mar: '03',
+  abr: '04',
+  mai: '05',
+  jun: '06',
+  jul: '07',
+  ago: '08',
+  set: '09',
+  out: '10',
+  nov: '11',
+  dez: '12',
+};
+
 class ReportRepository {
   async searchGoal(id, month, year) {
     const goal = await connection('goals')
@@ -15,10 +30,28 @@ class ReportRepository {
 
   async searchRecords(id, { month_start, year_start, month_end, year_end }) {
     const records = await connection('records')
-      .whereBetween('month', [month_start, month_end])
-      .whereBetween('year', [year_start, year_end])
+      .whereRaw(
+        `
+        to_date(concat(year, '/', (
+          CASE month
+            WHEN 'jan' then '01'
+            when 'fev' then '02'
+            when 'mar' then '03'
+            when 'abr' then '04'
+            when 'mai' then '05'
+            when 'jun' then '06'
+            when 'jul' then '07'
+            when 'ago' then '08'
+            when 'set' then '09'
+            when 'out' then '10'
+            when 'nov' then '11'
+            when 'dez' then '12'
+          end
+        ), '/', '01'
+      ), 'YYYY-MM-DD') BETWEEN '${year_start}-${months[month_start]}-01' AND '${year_end}-${months[month_end]}-01'
+      `
+      )
       .andWhere({ customer_id: id })
-
       .orderBy([
         { column: 'year', order: 'asc' },
         { column: 'month', order: 'asc' },

a9a9958e421cf5602dfb0d82fa1cc6b4bb554cf4
Leonardo Braz <lhleonardo05@gmail.com>
2020-11-03 07:54:37 -0400

    Refactoring

--- web/src/pages/Customer/Report/index.js
+++ web/src/pages/Customer/Report/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { FiFileText, FiLayout } from 'react-icons/fi';

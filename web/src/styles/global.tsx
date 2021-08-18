a25e82a7ba28a1874c32d6e12baf2e5fab19ba62
LMaroto <lucasmarotoga@gmail.com>
2021-08-18 10:10:09 -0400

    Criação de estilos globais e ajustes gerais

---
+++ web/src/styles/global.tsx
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+*:focus{
+  outline: 0;
+}
+
+html, body, #root{
+  height: 100vh;
+}
+
+body{
+  display: flex;
+  flex-direction: column;
+  -webkit-font-smoothing: antialiased;
+
+  background: #ff7518;
+}
+
+body, input, button, textarea{
+  font: 14px 'Roboto', sans-serif;
+}
+
+a{
+  text-decoration: none;
+}
+
+ul{
+  list-style: none;
+}
+
+button{
+  cursor: pointer;
+}
+
+@media print{
+  body{
+    background: #fff;
+  }
+}
+
+
+
+
+`;

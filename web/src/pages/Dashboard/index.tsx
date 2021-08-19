c00a44832d734645bbc36c7979e328f96580cb80
LMaroto <lucasmarotoga@gmail.com>
2021-08-19 09:39:34 -0400

    Início da construção do Dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FiUsers, FiFileText } from 'react-icons/fi';
 
 import Header from '../../components/Header';
 
+import { Container, OptionContainer, Option } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container>
+        <OptionContainer>
+          <Option to="/customers">
+            <FiUsers size={50} />
+            <h2>Clientes</h2>
+          </Option>
+          <Option to="/" color="#7159c1">
+            <FiFileText size={50} />
+          </Option>
+        </OptionContainer>
+      </Container>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ web/src/pages/Dashboard/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  height: calc(100vh - 1rem);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding-top: 2rem;
+  align-items: center;
+  background: #ff7518;
+`;
+
+export const OptionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: fit-content;
+  margin-top: 0.5rem;
+  background: none;
+`;
+
+export const Option = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.2rem;
+  width: 11rem;
+  height: 11rem;
+  margin: 0.5rem;
+  padding: 0.3rem;
+
+  background: ${props => (props.color ? props.color : 'white')};
+  color: ${props => (props.color ? '#fff' : '#ff7518')};
+  transition: transform 0.2s;
+
+  svg {
+    margin-bottom: 0.5rem;
+  }
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
